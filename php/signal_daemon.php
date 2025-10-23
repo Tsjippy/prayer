53fe2f503269831c3dcd445a2c64882e9ec2a81c
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-10-23 16:43:56 +0100

    Update signal_daemon.php

--- php/signal_daemon.php
+++ php/signal_daemon.php
@@ -4,8 +4,12 @@ use SIM;
 
 add_filter('sim-signal-daemon-response', __NAMESPACE__.'\addPrayerResponse', 10, 6);
 function addPrayerResponse($response, $message, $source, $users, $name, $signal){
-    if(str_starts_with($message, 'update prayer')){
+    if(str_starts_with($message, 'update prayer correct')){
         $response['message']    = updatePrayerRequest($message, $users, $signal);
+    }
+    
+    elseif(str_starts_with($message, 'update prayer')){
+        $response['message']    = checkPrayerRequestToUpdate($message, $users, $signal);
     }elseif(str_contains($message, 'prayer') && $name){
         $prayerRequest  = prayerRequest(true, true);
         $response['message']    = "This is the prayer for today:\n\n{$prayerRequest['message']}";
@@ -16,77 +20,60 @@ function addPrayerResponse($response, $message, $source, $users, $name, $signal)
 }
 
 function updatePrayerRequest($message, $users, $signal){
-    // perform the replacement
-    if($message == 'update prayer correct'){
-        // mark as updated for affected users
-        foreach($users as $user){
-            delete_user_meta($user->ID, 'pending-prayer-update');
-
-            $replacementData    = get_user_meta($user->ID, 'pending-prayer-update-data', true);
-            
-            delete_user_meta($user->ID, 'pending-prayer-update-data');
-
-            if(empty($replacementData)){
-                continue;
-            }
-        }
+    // mark as updated for affected users
+    foreach($users as $user){
+        $replacementData    = get_user_meta($user->ID, 'pending-prayer-update-data', true);
+        
+        delete_user_meta($user->ID, 'pending-prayer-update-data');
 
         if(empty($replacementData)){
-            return 'Something went wrong';
-        }
-
-        $post               = get_post($replacementData['post_id']);
-
-        if(empty($post)){
-            return "Post with id '{$replacementData->post_id}' not found";
-        }
-
-        $post->post_content = str_replace($replacementData['original'], $replacementData['replacement'], $post->post_content, $count);
-        
-        // do the actual replacement
-        wp_update_post(
-            $post,
-            false,
-            false
-        );
-
-        return "Replaced:\n'{$replacementData['message']}'\n\nwith:\n'{$replacementData['replacement']}'";
-    }
-
-
-    foreach($users as $user){
-        clean_user_cache($user);
-
-        $timeStamp      = get_user_meta($user->ID, 'pending-prayer-update', true);
-
-        if(!$timeStamp || !is_numeric($timeStamp)){
             continue;
         }
-
-        break;
     }
 
-    if(!$timeStamp || !is_numeric($timeStamp)){
-        return "You do not have a pending prayer request";
+    if(empty($replacementData)){
+        return 'Something went wrong';
     }
 
-    $sendMessage    = $signal->getSendMessageByTimestamp($timeStamp);
+    $post               = get_post($replacementData['post-id']);
 
-    if(!preg_match_all("/[\d]{2}-[\d]{2}-[\d]{4}/m", $sendMessage, $matches, PREG_SET_ORDER, 0)){
-        return "Not sure which prayer request is pending for you";
+    if(empty($post)){
+        return "Post with id '{$replacementData['post-id']}' not found";
     }
 
-    $replaceDate	= $matches[0][0];
+    $post->post_content = $replacementData['replacement'];
+    
+    // do the actual replacement
+    wp_update_post(
+        $post,
+        false,
+        false
+    );
 
-    // get the prayer request to be replaced
-    $prayer         = prayerRequest(false, false, $replaceDate);
+    return "Updated your prayer request for '{$replacementData['date']}'\n\nto:\n'{$replacementData['replacement']}'";
+}
+
+function checkPrayerRequestToUpdate($message, $users, $signal){
+    foreach($users as $user){
+        // get the prayer request to be replaced
+        $prayer         = get_post(
+            [
+                'post_type' => 'prayer',
+                'post_author' => $user->ID
+            ]
+        );
+        
+        if($prayer){
+            break;
+        }
+    }
     $prayer         = apply_filters('sim-prayer-request-to-update', $prayer, $replaceDate, $message);
 
     if(!$prayer){
         return "Could not find prayer request to update for $replaceDate";
     }
 
-    $prayerMessage  = trim($prayer['message']);
+    $prayerMessage  = trim($prayer->post_content);
 
     // confirm the replacement
     $replacetext    = trim(str_ireplace('update prayer', '', $message));
@@ -96,8 +83,6 @@ function updatePrayerRequest($message, $users, $signal){
             $user->ID, 
             'pending-prayer-update-data', 
             [
-                'original'      => $prayer['html'],
-                'message'       => $prayerMessage,
                 'replacement'   => $replacetext,
                 'post-id'       => $prayer['post']
             ]
