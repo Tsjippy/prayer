3858c7f37676c9fb30915a0efd39447ed0b6f234
Tsjippy <ewaldha@hotmail.com>
2026-06-23 20:30:22 +0200

    fixed: hook names

--- php/frontpage.php
+++ php/frontpage.php
@@ -8,7 +8,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-add_action('tsjippy-frontpage-before-main-content', __NAMESPACE__ . '\beforeMainContent', 5);
+add_action('tsjippy-theme-frontpage-before-main-content', __NAMESPACE__ . '\beforeMainContent', 5);
 function beforeMainContent()
 {
     if (!is_user_logged_in()) {
